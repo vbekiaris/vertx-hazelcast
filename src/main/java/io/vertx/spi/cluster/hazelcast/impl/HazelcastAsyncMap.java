c763518079d777732a5013d90b183b997ef0dde8
Jaromir Hamala <jaromir.hamala@gmail.com>
2016-03-14 20:49:47 +0200

    WIP: Use non-blocking method for put/get (cherry picked from commit 2637471)

--- src/main/java/io/vertx/spi/cluster/hazelcast/impl/HazelcastAsyncMap.java
+++ src/main/java/io/vertx/spi/cluster/hazelcast/impl/HazelcastAsyncMap.java
@@ -16,6 +16,7 @@
 
 package io.vertx.spi.cluster.hazelcast.impl;
 
+import com.hazelcast.core.ICompletableFuture;
 import com.hazelcast.core.IMap;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
@@ -30,6 +31,9 @@ import io.vertx.core.shareddata.impl.ClusterSerializable;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import static io.vertx.spi.cluster.hazelcast.impl.ConversionUtils.convertParam;
+import static io.vertx.spi.cluster.hazelcast.impl.ConversionUtils.convertReturn;
+
 public class HazelcastAsyncMap<K, V> implements AsyncMap<K, V> {
 
   private final Vertx vertx;
@@ -42,18 +46,22 @@ public class HazelcastAsyncMap<K, V> implements AsyncMap<K, V> {
 
   @Override
   public void get(K k, Handler<AsyncResult<V>> asyncResultHandler) {
-    K kk = convertParam(k);
-    vertx.executeBlocking(fut -> fut.complete(convertReturn(map.get(kk))), asyncResultHandler);
+    ICompletableFuture<V> future = (ICompletableFuture<V>) map.getAsync(convertParam(k));
+    future.andThen(
+            new HandlerCallBackAdapter<>(asyncResultHandler),
+            new VertxExecutorAdapter(vertx.getOrCreateContext())
+    );
   }
 
   @Override
   public void put(K k, V v, Handler<AsyncResult<Void>> completionHandler) {
     K kk = convertParam(k);
     V vv = convertParam(v);
-    vertx.executeBlocking(fut -> {
-      map.set(kk, HazelcastServerID.convertServerID(vv));
-      fut.complete();
-    }, completionHandler);
+    ICompletableFuture<Void> future = (ICompletableFuture<Void>) map.putAsync(kk, vv);
+    future.andThen(
+            new VoidHandlerCallBackAdapter(completionHandler),
+            new VertxExecutorAdapter(vertx.getOrCreateContext())
+    );
   }
 
   @Override
@@ -121,83 +129,6 @@ public class HazelcastAsyncMap<K, V> implements AsyncMap<K, V> {
   @Override
   public void size(Handler<AsyncResult<Integer>> resultHandler) {
     vertx.executeBlocking(fut -> fut.complete(map.size()), resultHandler);
-  }
-
-  @SuppressWarnings("unchecked")
-  private <T> T convertParam(T obj) {
-    if (obj instanceof ClusterSerializable) {
-      ClusterSerializable cobj = (ClusterSerializable)obj;
-      return (T)(new DataSerializableHolder(cobj));
-    } else {
-      return obj;
-    }
-  }
-
-  @SuppressWarnings("unchecked")
-  private <T> T convertReturn(Object obj) {
-    if (obj instanceof DataSerializableHolder) {
-      DataSerializableHolder cobj = (DataSerializableHolder)obj;
-      return (T)cobj.clusterSerializable();
-    } else {
-      return (T)obj;
-    }
-  }
-
-  private static final class DataSerializableHolder implements DataSerializable {
-
-    private ClusterSerializable clusterSerializable;
-
-    public DataSerializableHolder() {
-    }
-
-    private DataSerializableHolder(ClusterSerializable clusterSerializable) {
-      this.clusterSerializable = clusterSerializable;
-    }
-
-    @Override
-    public void writeData(ObjectDataOutput objectDataOutput) throws IOException {
-      objectDataOutput.writeUTF(clusterSerializable.getClass().getName());
-      Buffer buffer = Buffer.buffer();
-      clusterSerializable.writeToBuffer(buffer);
-      byte[] bytes = buffer.getBytes();
-      objectDataOutput.writeInt(bytes.length);
-      objectDataOutput.write(bytes);
-    }
-
-    @Override
-    public void readData(ObjectDataInput objectDataInput) throws IOException {
-      String className = objectDataInput.readUTF();
-      int length = objectDataInput.readInt();
-      byte[] bytes = new byte[length];
-      objectDataInput.readFully(bytes);
-      try {
-        Class<?> clazz = Thread.currentThread().getContextClassLoader().loadClass(className);
-        clusterSerializable = (ClusterSerializable)clazz.newInstance();
-        clusterSerializable.readFromBuffer(0, Buffer.buffer(bytes));
-      } catch (Exception e) {
-        throw new IllegalStateException("Failed to load class " + e.getMessage(), e);
-      }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) return true;
-      if (!(o instanceof DataSerializableHolder)) return false;
-      DataSerializableHolder that = (DataSerializableHolder) o;
-      if (clusterSerializable != null ? !clusterSerializable.equals(that.clusterSerializable) : that.clusterSerializable != null) {
-        return false;
-      }
-      return true;
-    }
-
-    @Override
-    public int hashCode() {
-      return clusterSerializable != null ? clusterSerializable.hashCode() : 0;
-    }
-
-    public ClusterSerializable clusterSerializable() {
-      return clusterSerializable;
-    }
   }
 
 
